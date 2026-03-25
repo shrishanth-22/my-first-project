0005fb3274af0cae02a0b10dd1ed2f4e8eff3aa3
Shrishanth Nellutla <bt25csa005@iiitn.ac.in>
2026-03-25 15:29:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello github!")
+print("1. Play Guess Game")
+print("2. Exit")
+
+choice = input("Enter choice: ")
+
+if choice == "1":
+    import guessGame
+elif choice == "2":
+    print("Bye!")

1b7330ebf009fa68b93b7d91bcc0f32b8028be52
Shrishanth Nellutla <bt25csa005@iiitn.ac.in>
2026-03-25 15:35:33 +0530

    Update guessGame.py

--- guessGame.py
+++ guessGame.py
@@ -2,7 +2,7 @@ import random
 
 secret_num = random.randint(1,9)
 guess_count = 0
-guess_limit = 5
+guess_limit = 3
 while guess_count < guess_limit:
     guess = int(input('guess number between 1 to 9 : '))
     guess_count += 1

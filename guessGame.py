b01b14be3ca59380dc622e373a49a75f551e9656
Shrishanth Nellutla <bt25csa005@iiitn.ac.in>
2026-03-25 15:53:10 +0530

    Update guessGame.py

--- guessGame.py
+++ guessGame.py
@@ -1,18 +1,23 @@
 import random
 
-secret_num = random.randint(1,9)
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input('guess number between 1 to 9 : '))
-    guess_count += 1
-    if guess == secret_num:
-        print('You Won!🎉')
+while True:
+    secret_num = random.randint(1,9)
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:
+        guess = int(input('guess number between 1 to 9 : '))
+        guess_count += 1
+        if guess == secret_num:
+            print('You Won!🎉')
+            break
+        elif guess < secret_num:
+            print("Too low")
+        elif guess > secret_num:
+            print("Too high")
+    else:
+        print('you lost')
+        print('correct number is ' + str(secret_num))
+    again = input("wanna play again (y/n)")
+    if again.lower() != "y":
         break
-    elif guess < secret_num:
-        print("Too low")
-    elif guess > secret_num:
-        print("Too high")
-else:
-    print('you lost')
-    print('correct number is ' + str(secret_num))
+        
